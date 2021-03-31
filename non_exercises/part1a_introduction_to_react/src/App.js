f3ebe9317f50963f3b134be37dd0908395d4c898
NikolayM <6795662+in7l@users.noreply.github.com>
2021-03-31 23:41:18 +0300

    Apply changes according to course instructions in part 1c: stateful component, event handling.

--- non_exercises/part1a_introduction_to_react/src/App.js
+++ non_exercises/part1a_introduction_to_react/src/App.js
@@ -1,30 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+const App = () => {
+  const [ counter, setCounter ]  = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you were probably born in {bornYear()}
-      </p>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
     </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter';
-  const age = 10;
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
   );
 };
 
