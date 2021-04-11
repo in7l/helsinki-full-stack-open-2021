63c9afb112b2a5a0fee04ebba9affda72dc630b0
NikolayM <6795662+in7l@users.noreply.github.com>
2021-04-11 11:28:29 +0300

    Exercise 1.12: Implement anecdotes app with a button that changes to a random anecdote.

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
+   */
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  };
+
+  const switchAnecdote = () => {
+    if (anecdotes.length > 1) {
+      let newIndex;
+      // This loop is needed to make sure the anecdote definitely changes on each click.
+      do {
+        newIndex = getRandomInt(0, anecdotes.length);
+      } while (newIndex === selected);
+
+      setSelected(newIndex);
+    }
+  };
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br />
+      <button onClick={switchAnecdote}>next anecdote</button>
+    </div>
+  );
+};
+
+export default App;
