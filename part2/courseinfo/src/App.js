84a91d4149da1cfed0e25f4d46c2500b11559ffa
NikolayM <6795662+in7l@users.noreply.github.com>
2021-04-11 15:05:06 +0300

    Exercise 2.5: Move the Course component to its own file, along with its subcomponents.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,44 +1,5 @@
 import React from 'react';
-
-const Header = (props) => (
-  <h2>{props.course}</h2>
-);
-
-const Part = (props) => (
-  <p>
-    {props.part} {props.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const CourseTotalExercises = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, { exercises }) => sum + exercises, 0);
-
-  return (
-    <p>
-      <strong>total of {totalExercises} exercises</strong>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <CourseTotalExercises parts={course.parts} />
-    </div>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
