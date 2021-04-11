9b5fc7da8ee131601533b8a824da9ae2d06139b8
NikolayM <6795662+in7l@users.noreply.github.com>
2021-04-11 15:03:19 +0300

    Exercise 2.4: Add support for multiple Courses.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = (props) => (
-  <h1>{props.course}</h1>
+  <h2>{props.course}</h2>
 );
 
 const Part = (props) => (
@@ -41,34 +41,59 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  };
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )}
+    </div>
+  );
 };
 
 export default App;
