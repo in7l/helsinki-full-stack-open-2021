5ac1b264ca2a689f05adc2ca5fc1058dffee325f
NikolayM <6795662+in7l@users.noreply.github.com>
2021-07-15 23:54:17 +0300

    Part 3a: Implement POST /api/notes route. Define requests file to be used by VSCode REST Client extension.

--- non_exercises/part3/backend/index.js
+++ non_exercises/part3/backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -29,6 +31,34 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+  return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+  const { body } = request;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
 });
 
 app.get('/api/notes/:id', (request, response) => {
