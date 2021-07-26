73ae5d02a0509cd30d9045729e886f5c9679a89e
NikolayM <6795662+in7l@users.noreply.github.com>
2021-07-26 21:36:47 +0300

    Part 3c: Delete from mongodb the notes when a DELETE request is received. (not instructed to be implemented)

--- non_exercises/part3/backend/index.js
+++ non_exercises/part3/backend/index.js
@@ -23,27 +23,6 @@ app.use(requestLogger);
 
 app.use(express.static('build'));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-];
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello world</h1>');
 });
@@ -94,10 +73,10 @@ app.get('/api/notes/:id', (request, response) => {
 });
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+  const { id } = request.params;
+  Note.findByIdAndDelete(id).then(result => {
+    response.status(204).end();
+  })
 });
 
 const unknownEndpoint = (request, response) => {
